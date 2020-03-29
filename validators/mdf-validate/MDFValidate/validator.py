a8c01cec44b96e9c876d888e674469c258d6669a
Mark A. Jensen <mark.jensen@nih.gov>
2020-03-28 23:27:49 -0400

    update-py - 2to3 on validator.py

--- validators/mdf-validate/MDFValidate/validator.py
+++ validators/mdf-validate/MDFValidate/validator.py
@@ -19,7 +19,7 @@ def construct_mapping(self, node, deep=False):
     key = self.construct_object(key_node, deep=deep)
     try:
       hash(key)
-    except TypeError, exc:
+    except TypeError as exc:
       raise ConstructorError("while constructing a mapping", node.start_mark,
                                "found unacceptable key (%s)" % exc, key_node.start_mark)
     value = self.construct_object(value_node, deep=deep)
@@ -40,7 +40,7 @@ class MDFValidator:
     self.yloader.construct_mapping = construct_mapping # monkey patch to detect dup keys
 
   def yaml_validate(self):
-    print "Checking schema YAML ====="
+    print("Checking schema YAML =====")
     try:
       sch_file = open(self.sch_filename,"r")
     except IOError:
@@ -48,29 +48,29 @@ class MDFValidator:
     try:
       self.schema = yaml.load(sch_file, Loader=self.yloader)
     except ConstructorError as ce:
-      print "YAML error in '{fn}':\n{e}".format(fn=self.sch_filename,e=ce)
+      print("YAML error in '{fn}':\n{e}".format(fn=self.sch_filename,e=ce))
       return ce
     except ParserError as e:
-      print "YAML error in '{fn}':\n{e}".format(fn=self.sch_filename,e=e)
+      print("YAML error in '{fn}':\n{e}".format(fn=self.sch_filename,e=e))
       return e
     except Exception:
       raise
 
     if (self.inst_filenames):
-      print "Checking instance YAML ====="
+      print("Checking instance YAML =====")
       for inst_filename in self.inst_filenames:
         try:
           inst_file = open(inst_filename,"r")
         except IOError as e:
-          print "Can't open '{fn}' ({e}), skipping".format(fn=inst_filename,e=e)
+          print("Can't open '{fn}' ({e}), skipping".format(fn=inst_filename,e=e))
         try:
           inst_yaml=yaml.load(inst_file, Loader=self.yloader)
           self.instance.update(inst_yaml)
         except ConstructorError as ce:
-          print "YAML error in '{fn}':\n{e}".format(fn=inst_filename,e=ce)
+          print("YAML error in '{fn}':\n{e}".format(fn=inst_filename,e=ce))
           return ce
         except ParserError as e:
-          print "YAML error in '{fn}':\n{e}".format(fn=inst_filename,e=e)
+          print("YAML error in '{fn}':\n{e}".format(fn=inst_filename,e=e))
           return e
         except Exception:
           raise
@@ -79,27 +79,27 @@ class MDFValidator:
     
   def schema_validate(self):
     if not self.yaml_valid:
-      print "YAML invalid - not checking schema"
+      print("YAML invalid - not checking schema")
       return "YAMLInvalid"
-    print "Checking JSON schema ====="
+    print("Checking JSON schema =====")
     try:
       d6.check_schema(self.schema)
     except SchemaError as se:
-      print se
+      print(se)
       return se
     if (self.instance):
-      print "Checking instance against schema ====="
+      print("Checking instance against schema =====")
       try:
         validate(instance=self.instance.as_dict(), schema=self.schema)
       except ConstructorError as ce:
-        print ce
+        print(ce)
         return ce
       except RefResolutionError as re:
-        print re
+        print(re)
         return re
       except ValidationError as ve:
         for e in d6(self.schema).iter_errors(self.instance.as_dict()):
-          print e;
+          print(e);
         return ve
     return None
   def test_yaml_valid(self):
