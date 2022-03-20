d193ede802f24a36d8bca76c9680e607e0ec5de7
Mark A. Jensen <mark.jensen@nih.gov>
2022-03-19 21:02:59 -0400

    feat(build): use setup.cfg
    include .../dist with tar.gz and .whl

--- validators/mdf-validate/setup.py
+++ validators/mdf-validate/setup.py
@@ -1,18 +1,2 @@
-from setuptools import setup, find_packages
-setup(
-  name="mdf-validate",
-  version="0.3",
-  packages=find_packages(),
-  scripts=['test-mdf.py'],
-  install_requires=[
-    'jsonschema>=3.0.1',
-    'PyYAML>=5.1.1',
-    'delfick-project',
-    'requests'
-    ],
-  tests_require=[
-    'jsonschema>=3.0.1',
-    'PyYAML>=5.1.1',
-    'pytest'
-    ]
-  )
+from setuptools import setup
+setup()
