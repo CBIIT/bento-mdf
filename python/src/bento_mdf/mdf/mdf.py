c5932081e77ffc3ce88a229513db48cf894ade6c
Nelson Moore <nelson.moore@essential-soft.com>
2024-09-16 12:38:48 -0400

    style(mdf.py): formatting

--- python/src/bento_mdf/mdf/mdf.py
+++ python/src/bento_mdf/mdf/mdf.py
@@ -1,38 +1,41 @@
 """
-bento_mdf.mdf
-==============
+Load and write Model Description Format (MDF) files to & from bento-meta Models.
 
 This module contains :class:`MDF`, a class for reading a graph data model in
 Model Description Format into a :class:`bento_meta.model.Model` object, and
 writing the opposite way.
-
 """
+
+from __future__ import annotations
+
 import logging
 import re
 import sys
 from collections import ChainMap
 from tempfile import TemporaryFile
-from typing import Dict
 
 import requests
-from .convert import spec_to_entity
-from ..validator import MDFValidator
 from bento_meta.entity import ArgError
 from bento_meta.model import Model
-from bento_meta.objects import Edge, Node, Property, Tag, Term, ValueSet
+from bento_meta.objects import Edge, Node, Property, Term
 from nanoid import generate
 from tqdm import tqdm
-from pdb import set_trace
+
+from bento_mdf.mdf.convert import spec_to_entity
+from bento_mdf.validator import MDFValidator
+
 sys.path.extend([".", ".."])
 
 
-def make_nano():
+def make_nano() -> str:
+    """Generate a 6-character alphanumeric string."""
     return generate(
-        size=6, alphabet="abcdefghijkmnopqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ0123456789"
+        size=6,
+        alphabet="abcdefghijkmnopqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ0123456789",
     )
 
 
-class MDF(object):
+class MDF:
     def __init__(
         self,
         *yaml_files,
@@ -43,14 +46,16 @@ class MDF(object):
         raiseError=False,
         logger=logging.getLogger(__name__),
     ):
-        """Create a :class:`Model` from MDF YAML files/Write a :class:`Model` to YAML
+        """
+        Create a :class:`Model` from MDF YAML files/Write a :class:`Model` to YAML
         :param str|file|url *yaml_files: MDF filenames or file objects,
         in desired merge order
         :param str handle: Handle (name) for the resulting Model
         :param :class:`Model` model: Model to convert to MDF
         :param boolean raiseError: raise on error if True
         :param :class:`logging.Logger` logger: Python logger (suitable default)
-        :attribute model: the :class:`bento_meta.model.Model` created"""
+        :attribute model: the :class:`bento_meta.model.Model` created
+        """
         if model and not isinstance(model, Model):
             raise ArgError("arg model= must be a Model instance")
 
@@ -72,14 +77,15 @@ class MDF(object):
         if self.files:
             self.load_yaml()
             self.create_model(raiseError=raiseError)
-        else:
-            if not model:
-                self.logger.warning("No MDF files or model provided to constructor")
+        elif not model:
+            self.logger.warning("No MDF files or model provided to constructor")
 
     @property
     def model(self):
-        """The :class:`bento_meta.model.Model` object created from the
-        MDF input"""
+        """
+        The :class:`bento_meta.model.Model` object created from the
+        MDF input
+        """
         return self._model
 
     def load_yaml(self, verify=True):
@@ -91,14 +97,10 @@ class MDF(object):
                     response = requests.get(f, verify=verify)
                     if not response.ok:
                         self.logger.error(
-                            "Fetching url {} returned code {}".format(
-                                response.url, response.status_code
-                            )
+                            f"Fetching url {response.url} returned code {response.status_code}",
                         )
                         raise ArgError(
-                            "Fetching url {} returned code {}".format(
-                                response.url, response.status_code
-                            )
+                            f"Fetching url {response.url} returned code {response.status_code}",
                         )
                     response.encoding = "utf8"
                     fh = TemporaryFile()
@@ -107,21 +109,22 @@ class MDF(object):
                     fh.seek(0)
                     vargs.append(fh)
                 else:
-                    fh = open(f, "r")
+                    fh = open(f)
                     vargs.append(fh)
             else:
                 vargs.append(f)
 
-        v = MDFValidator(self.mdf_schema, *vargs, raiseError=True)
+        v = MDFValidator(self.mdf_schema, *vargs, raise_error=True)
         self.mdf_schema = v.load_and_validate_schema()
         self.mdf = v.load_and_validate_yaml()
         self.mdf = self.mdf.as_dict()
 
     def create_model(self, raiseError=False):
-        """Create :class:`Model` instance from loaded YAML
+        """
+        Create :class:`Model` instance from loaded YAML
 
         Handling enumerated acceptable values ("value sets"):
-        
+
         If the input MDF does not have a Terms: section, the strings listed under
         an Enum: key are interpreted as literal Term values (i.e., the expected data),
         and the Origin of each Term is set as the model handle (i.e.,the model is itself
@@ -132,9 +135,10 @@ class MDF(object):
         in the the Terms: section. If found, the definition in the Terms: section is used.
         If no referenced Term is found, then the string is treated
         as a literal Term value with the model as origin, as in the previous paragraph.
-        
+
         :param boolean raiseError: Raise if MDF errors found
-        Note: This is brittle, since the syntax of MDF is hard-coded into this method."""
+        Note: This is brittle, since the syntax of MDF is hard-coded into this method.
+        """
         success = True
         if not self.mdf.keys():
             raise ValueError("attribute 'mdf' not set - are yamls loaded?")
@@ -143,14 +147,14 @@ class MDF(object):
         if self.mdf.get("URI"):
             self.uri = self.mdf["URI"]
         if self.handle:
-            self._model = Model(handle=self.handle,
-                                version=self.version,
-                                uri=self.uri)
+            self._model = Model(handle=self.handle, version=self.version, uri=self.uri)
         elif self.mdf.get("Handle"):
             self.handle = self.mdf["Handle"]
-            self._model = Model(handle=self.mdf["Handle"],
-                                version=self.version,
-                                uri=self.uri)
+            self._model = Model(
+                handle=self.mdf["Handle"],
+                version=self.version,
+                uri=self.uri,
+            )
         else:
             self.logger.error("Model handle not present in MDF nor provided in args")
             success = False
@@ -159,32 +163,32 @@ class MDF(object):
         yunps = self.mdf.get("UniversalNodeProperties")
         yurps = self.mdf.get("UniversalRelationshipProperties")
 
-
         # create terms first, if any -- properties depend on these
         if "Terms" in self.mdf:
             for t_hdl in tqdm(self.mdf["Terms"]):
                 spec = self.mdf["Terms"][t_hdl]
                 if "Value" not in spec:
                     self.logger.error(
-                        f"Term specs must have a Value key and a non-null string value (term '{t_hdl}')"
+                        f"Term specs must have a Value key and a non-null string value (term '{t_hdl}')",
                     )
                     success = False
                 if "Origin" not in spec:
                     self.logger.warning(
-                        f"No Origin provided for term '{t_hdl}'"
+                        f"No Origin provided for term '{t_hdl}'",
                     )
-                term = spec_to_entity(t_hdl, spec,
-                                      {"_commit": self._commit},
-                                      Term)
+                term = spec_to_entity(t_hdl, spec, {"_commit": self._commit}, Term)
                 self._terms[term.handle] = term
 
         # create nodes
         for n in self.mdf["Nodes"]:
             spec = self.mdf["Nodes"][n]
             node = self._model.add_node(
-                spec_to_entity(n, spec, 
-                               {"model": self.handle, "_commit": self._commit},
-                               Node)
+                spec_to_entity(
+                    n,
+                    spec,
+                    {"model": self.handle, "_commit": self._commit},
+                    Node,
+                ),
             )
             if "Term" in spec:
                 self.annotate_entity_from_mdf(node, spec["Term"])
@@ -197,16 +201,20 @@ class MDF(object):
                     self.logger.warning(
                         "No node '{src}' defined for edge "
                         "spec '{ename}' from '{src}' to '{dst}'".format(
-                            src=ends["Src"], dst=ends["Dst"], ename=e
-                        )
+                            src=ends["Src"],
+                            dst=ends["Dst"],
+                            ename=e,
+                        ),
                     )
                     continue
                 if ends["Dst"] not in self._model.nodes:
                     self.logger.warning(
                         "No node '{dst}' defined for edge "
                         "spec '{ename}' from '{src}' to '{dst}'".format(
-                            src=ends["Src"], dst=ends["Dst"], ename=e
-                        )
+                            src=ends["Src"],
+                            dst=ends["Dst"],
+                            ename=e,
+                        ),
                     )
                     continue
                 init = {
@@ -222,8 +230,10 @@ class MDF(object):
                     self.logger.warning(
                         "edge '{ename}' from '{src}' to '{dst}' "
                         "does not specify a multiplicity".format(
-                            ename=e, src=ends["Src"], dst=ends["Dst"]
-                        )
+                            ename=e,
+                            src=ends["Src"],
+                            dst=ends["Dst"],
+                        ),
                     )
                     init["multiplicity"] = Edge.default("multiplicity")
                 if init["multiplicity"] not in (
@@ -239,14 +249,14 @@ class MDF(object):
                             src=ends["Src"],
                             dst=ends["Dst"],
                             mult=init["multiplicity"],
-                        )
+                        ),
                     )
                 # if tags are set in the Ends entry, these will be attached
                 # to the Edge, _rather than_ tags set in the "edge key" or spec
                 # level
                 spec["Tags"] = ends.get("Tags") or spec.get("Tags")
                 edge = self._model.add_edge(
-                    spec_to_entity(e, spec, init, Edge)
+                    spec_to_entity(e, spec, init, Edge),
                 )
                 term = ends.get("Term") or spec.get("Term")
                 if term:
@@ -273,8 +283,8 @@ class MDF(object):
                 ]
                 if len(ends) > 1:
                     self.logger.warning(
-                        "edge '{ename}' has more than one Ends pair Src:'{src}',"
-                        "Dst:'{dst}'".format(ename=hdl, src=src, dst=dst)
+                        f"edge '{hdl}' has more than one Ends pair Src:'{src}',"
+                        f"Dst:'{dst}'",
                     )
                 end = ends[0]
 
@@ -288,9 +298,7 @@ class MDF(object):
                     propnames[ent] = pnames
             else:
                 self.logger.error(
-                    "Unhandled entity type {type} for properties".format(
-                        type=type(ent).__name__
-                    )
+                    f"Unhandled entity type {type(ent).__name__} for properties",
                 )
                 success = False
         prop_of = {}
@@ -300,7 +308,7 @@ class MDF(object):
                     prop_of[p].append(ent)
                 else:
                     prop_of[p] = [ent]
-        propdefs = self.mdf["PropDefinitions"]                    
+        propdefs = self.mdf["PropDefinitions"]
         defns_for = set(propdefs.keys())
         for pname in prop_of:
             for ent in prop_of[pname]:
@@ -316,30 +324,29 @@ class MDF(object):
                     spec = propdefs.get(pname)
                 if not spec:
                     self.logger.warning(
-                        "property '{pname}' does not have a corresponding "
-                        "propdef for entity '{handle}'".format(
-                            pname=pname, handle=ent.handle
-                        )
+                        f"property '{pname}' does not have a corresponding "
+                        f"propdef for entity '{ent.handle}'",
                     )
                     continue
-                else:
-                    if key in defns_for:
-                        defns_for.remove(key)
+                elif key in defns_for:
+                    defns_for.remove(key)
                 prop = self.create_or_merge_prop_from_mdf(
-                    spec, p_hdl=pname, force_create=force
+                    spec,
+                    p_hdl=pname,
+                    force_create=force,
                 )
                 self._model.add_prop(ent, prop)
                 ent.props[prop.handle] = prop
         if defns_for:
             self.logger.warning(
                 "No properties in model corresponding to the following "
-                "PropDefintions: {}".format(defns_for)
+                f"PropDefintions: {defns_for}",
             )
         if raiseError and not success:
             raise RuntimeError("MDF errors found; see log output.")
 
         # now add the terms collected in self._terms to the model (self._model)...
-        
+
         return self._model
 
     def create_or_merge_prop_from_mdf(self, spec, p_hdl, force_create):
@@ -347,10 +354,12 @@ class MDF(object):
             pass
         else:
             prop = spec_to_entity(
-                p_hdl, spec,
+                p_hdl,
+                spec,
                 {"handle": p_hdl, "model": self.handle, "_commit": self._commit},
-                Property)
-            if (prop.value_set and prop.value_set._commit == "dummy"):
+                Property,
+            )
+            if prop.value_set and prop.value_set._commit == "dummy":
                 terms = []
                 # merge terms references in enums into terms defined
                 # in a separate Terms: section
@@ -362,8 +371,8 @@ class MDF(object):
                 # allow bento_meta.model machinery to create
                 # the actual value_set object and store
                 # terms in Model object:
-                self._model.add_terms(prop, terms)
-            
+                self._model.add_terms(prop, *terms)
+
             if "Term" in spec:
                 self.annotate_entity_from_mdf(prop, spec["Term"])
             if force_create:
@@ -375,17 +384,15 @@ class MDF(object):
         for spec in yterm_list:
             if "Value" not in spec:
                 self.logger.error(
-                    f"Term specs must have a Value key and a non-null string value, in entity '{ent.handle}'"
+                    f"Term specs must have a Value key and a non-null string value, in entity '{ent.handle}'",
                 )
                 return False
             if "Origin" not in spec:
                 self.logger.warning(
-                    f"No Origin provided for Term annotation in entity '{ent.handle}'"
+                    f"No Origin provided for Term annotation in entity '{ent.handle}'",
                 )
                 spec["Origin"] = self.handle
-            term = spec_to_entity(None, spec,
-                                  {"_commit": self._commit},
-                                  Term)
+            term = spec_to_entity(None, spec, {"_commit": self._commit}, Term)
             # merge or record term
             if not self._annotations.get((term.handle, term.origin_name)):
                 self._annotations[(term.handle, term.origin_name)] = term
@@ -396,4 +403,3 @@ class MDF(object):
             if self._commit:
                 if not ent.concept._commit:
                     ent.concept._commit = self._commit
-
