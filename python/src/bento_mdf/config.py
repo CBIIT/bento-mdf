440dd826183290ed1ed01490fae42bd121ac278d
Mark A. Jensen <mark.jensen@nih.gov>
2026-06-16 15:42:53 -0400

    feat(edp): graceful workaround when STS_URL is not set
    CICD test should run ...

--- python/src/bento_mdf/config.py
+++ python/src/bento_mdf/config.py
@@ -1,7 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+from pydantic import Field, ValidationError
+import logging
+import re
+import os
+from pdb import set_trace
 
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARN)
 class Settings(BaseSettings):
     # STS
     sts_url: str = Field(..., alias="STS_URL")
@@ -14,4 +19,13 @@ class Settings(BaseSettings):
     )
 
 
-settings = Settings()
+try:
+    settings = Settings()
+except ValidationError as e:
+    if re.findall("STS_URL", str(e)):
+        logger.warn("STS_URL env not set: use .env or explicitly set; setting to 'http://localhost:8000/v2'")
+        os.environ['STS_URL'] = 'http://localhost:8000/v2'
+        settings = Settings()
+    else:
+        raise(e)
+
